7e3fc8c1518d6dc99ba5fda4b7216c4dfa7f419c
kavyadandugula999-max <kavyadandugula999@gmail.com>
2026-09-19 20:29:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-// script.js
-
 let display = document.getElementById("display");
 
-function appendValue(value) {
-    display.value += value;
+function addToDisplay(value) {
+
+    if (display.value === "0" || display.value === "Error") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
 }
 
 function clearDisplay() {
-    display.value = "";
+    display.value = "0";
 }
 
 function deleteLast() {
+
     display.value = display.value.slice(0, -1);
+
+    if (display.value === "") {
+        display.value = "0";
+    }
 }
 
 function calculate() {
+
     try {
-        // Evaluate the mathematical expression
-        display.value = eval(display.value);
+
+        let result = eval(display.value);
+
+        display.value = result;
+
     } catch (error) {
+
         display.value = "Error";
     }
 }
